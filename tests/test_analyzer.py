ba042d20f6bb4ab02190509fea4e5c42e38f780a
Flavio Di Natale <dinataleflavio@gmail.com>
2026-07-08 15:16:53 +0200

    make tests actually useful

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -1,6 +1,20 @@
-from move.analyzer import analize_gpx_file
+from move.analyzer import analize_gpx_file, Stay, DURATION_TRESHOLD
 
 
-def test_analize_file():
+def test_analize_produce_sequential_items():
     file = "./examples/20260705.gpx"
-    analize_gpx_file(file)
+    items = analize_gpx_file(file)
+    prev_item = items[0]
+
+    for item in items[1:]:
+        assert item.start.id == prev_item.end.id
+        prev_item = item
+
+
+def test_analize_dose_not_create_short_stays():
+    file = "./examples/20260705.gpx"
+    items = analize_gpx_file(file)
+
+    for item in items:
+        if type(item) is Stay:
+            assert item.duration() > DURATION_TRESHOLD
